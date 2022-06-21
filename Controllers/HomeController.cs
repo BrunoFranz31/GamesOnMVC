4fff3e52f5c1af1bdcc8ff31321c71173379f159
BrunoFranz31 <brunofranz2006@gmail.com>
2022-06-21 02:23:32 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TENTATIVA2.Models;
+
+namespace TENTATIVA2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+         public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+
+         public IActionResult Conta()
+        {
+            return View();
+        }
+
+         public IActionResult LoginA()
+        {
+            return View();
+        }
+
+           public IActionResult LoginP()
+        {
+            return View();
+        }
+
+           public IActionResult Produtos()
+        {
+            return View();
+        }
+
+           public IActionResult User()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
